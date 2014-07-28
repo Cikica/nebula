7b2852b93937c3f50010bcda5b0e86abf96b2ab0
Cikica <aleksandar.winter@gmail.com>
2014-07-28 11:59:18 +0100

    Fixed a way for base url loading and findin the root directory of the file which loads the module loader

--- entry.js
+++ entry.js
@@ -1,7 +1,17 @@
 window.nebula = (function ( nebula ) {
 
-	var begin_loading, get_the_root_directory_based_on_last_loaded_script, root_directory
+	var begin_loading, get_the_root_directory_based_on_last_loaded_script, root_directory, get_path_directory
 
+	get_path_directory = function ( path ) {
+		var split_path, split_directory_path
+		split_path           = path.split("/")
+		split_directory_path = split_path.slice( 0, split_path.length-1 )
+		if ( split_directory_path.length > 0 ) {
+			return split_directory_path.join("/")
+		} else { 
+			return null
+		}
+	},
 	get_the_root_directory_based_on_last_loaded_script = function () { 
 
 		var loaded_scripts, directory_from_attribute, last_loaded_script
@@ -9,12 +19,20 @@ window.nebula = (function ( nebula ) {
 		loaded_scripts           = document.getElementsByTagName("script")
 		last_loaded_script       = loaded_scripts[loaded_scripts.length-1]
 		directory_from_attribute = last_loaded_script.getAttribute("data-directory")
-		
 		if (  directory_from_attribute )  { 
 			return directory_from_attribute
 		} else {
-			var root_path
-			root_path = last_loaded_script.src.replace( last_loaded_script.getAttribute("src"), "" )
+			
+			var root_path, script_source_from_attribute
+
+			script_source_from_attribute = last_loaded_script.getAttribute("src")
+
+			if ( last_loaded_script.src === script_source_from_attribute ) {
+				return get_path_directory( script_source_from_attribute )
+			}
+			
+			root_path = last_loaded_script.src.replace( script_source_from_attribute, "" )
+
 			if ( root_path[root_path.length-1] === "/" ) {
 				return root_path.slice( 0, root_path.length-1 )
 			} else { 
@@ -24,6 +42,10 @@ window.nebula = (function ( nebula ) {
 	},
 	
 	begin_loading = function ( load ) {
+
+		require.config({
+			baseUrl : root_directory
+		})
 
 		requirejs([ 
 			load.directory +"/get.js", 
@@ -66,7 +88,7 @@ window.nebula = (function ( nebula ) {
 	} else {
 		var require_js
 		require_js        = document.createElement("script")
-		require_js.src    = "require.js"
+		require_js.src    = root_directory + "/require.js"
 		require_js.onload = function () {
 			begin_loading({
 				directory : root_directory
